d8fd6fa4eceef2bdcbafa3f582c0cda2502b6c30
wallstop <dbfsinfil@gmail.com>
2015-06-17 00:04:06 -0700

    Minor documentatin around serializatoinutils

--- src/utils/SerializationUtils.java
+++ src/utils/SerializationUtils.java
@@ -13,25 +13,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import core.utils.SerializationFactory;
 
 /**
- * Helper class to read & write our objects into and out of JSON
- * 
- * Throws RuntimeExceptions if it can't properly parse the JSON/class
- * 
- * @author wallstop
+ * Helper class to read & write our objects into and out of JSON Throws
+ * RuntimeExceptions if it can't properly parse the JSON/class
  *
+ * @author wallstop
  */
-public class SerializationUtils
+public final class SerializationUtils
 {
     private static final Logger LOG = LoggerFactory.getLogger(SerializationUtils.class);
 
     private static final ObjectMapper MAPPER = newDefaultMapper();
     static
     {
-        MAPPER.registerModule(SerializationFactory.newVector2SerializationModule());
-        MAPPER.registerModule(SerializationFactory.newPlayerSerializationModule());
-        MAPPER.registerModule(SerializationFactory.newScoreSerializationModule());
+        /*
+         * Set up core class serializers. While Jackson normally does a great
+         * job of automagically constructing classes, it has an edge case for
+         * Map Keys. As such, we need to specify exactly how to serialize &
+         * deserialize Map Keys for every core class, or we'll run into a ton of
+         * json parsing exceptions
+         */
+        registerModules(SerializationFactory.newVector2SerializationModule());
+        registerModules(SerializationFactory.newPlayerSerializationModule());
+        registerModules(SerializationFactory.newScoreSerializationModule());
     }
 
+    /**
+     * Returns an un-decorated ObjectMapper similar to the one that this class
+     * uses, but without the Modules. The visibility settings between
+     * SerializationUtil's internal mapper and this one will be similar.
+     *
+     * @return An ObjectMapper with configured visibilities.
+     */
     public static ObjectMapper newDefaultMapper()
     {
         final ObjectMapper defaultMapper = new ObjectMapper();
@@ -42,32 +54,51 @@ public class SerializationUtils
         return defaultMapper;
     }
 
+    /**
+     * @param modules
+     */
     public static void registerModules(final Module... modules)
     {
         Validate.notNull(modules, "Cannot register a null module!");
+        LOG.info("Registering modules {}", modules);
         MAPPER.registerModules(modules);
     }
 
+    /**
+     * @param json
+     * @param clazz
+     * @return
+     */
     public static <T> T readValue(final String json, final Class<T> clazz)
     {
         try
         {
             return MAPPER.readValue(json, clazz);
         }
-        catch(IOException e)
+        catch(final IOException e)
         {
             LOG.error("Could not convert {} into {}", new Object[] { json, clazz }, e);
             throw new IllegalArgumentException(e);
         }
     }
 
+    /**
+     * Converts the provided value to it's corresponding JSON string. Assumes
+     * that the provided value can be properly JSON-ified
+     *
+     * @param value
+     *            non-null value to turn into a JSON string
+     * @return Value as a JSON string
+     * @throws IllegalArgumentException
+     *             if the value could not be mapped to a JSON string.
+     */
     public static <T> String writeValue(final T value)
     {
         try
         {
             return MAPPER.writeValueAsString(value);
         }
-        catch(JsonProcessingException e)
+        catch(final JsonProcessingException e)
         {
             LOG.error("Could not convert {} into json", value, e);
             throw new IllegalArgumentException(e);
